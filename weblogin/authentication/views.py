69ba499c3d51fbf162fee50b77eca3691663825f
Valentin Romero <98521093+vromero777@users.noreply.github.com>
2022-09-26 10:44:31 +0200

    Initial Structure
    Docker Compose
    Authentication App and URLs
    Main Templates

---
+++ weblogin/authentication/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return HttpResponse("hello I am working")
+
+def signup(request):
+    return render(request, "authentication/signup.html")
+
+def signin(request):
+    return render(request, "authentication/signin.html")
+
+def signout(request):
+    pass
